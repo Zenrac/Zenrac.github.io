96f29b059723d177cbf68e119d6080b1967c1d68
Zenrac <zenrac@outlook.fr>
2021-08-15 16:00:27 +0200

    add https

--- js/mavanimenextpreviousepbuttons.user.js
+++ js/mavanimenextpreviousepbuttons.user.js
@@ -5,6 +5,7 @@
 // @description  try to take over the world!
 // @author       Zenrac
 // @match        http://www.mavanimes.co/*
+// @match        https://www.mavanimes.co/*
 // @icon         https://www.google.com/s2/favicons?domain=mavanimes.co
 // @grant        none
 // ==/UserScript==
