6b86aa9bc441a5b99a2b2034cfd1d5ea12a9af7e
Zenrac <zenrac@outlook.fr>
2021-08-15 15:29:49 +0200

    Update and rename js/adkVfRemover.user.js to BetterADK.user.js

---
+++ BetterADK.user.js
@@ -0,0 +1,87 @@
+// ==UserScript==
+// @name         BetterADK (Remove VF & Mal Buttons)
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       Zenrac
+// @match        https://www.adkami.com/*
+// @match        https://www.adkami.com/*
+// @match        https://m.adkami.com/*
+// @match        http://www.adkami.com/*
+// @match        http://m.adkami.com/*
+// @downloadURL  https://raw.githubusercontent.com/Zenrac/Zenrac.github.io/main/js/adkVfRemover.user.js
+// @homepageURL  https://github.com/zenrac/Zenrac.github.io
+// @supportURL   https://github.com/zenrac/Zenrac.github.io/issues
+// @icon         https://www.google.com/s2/favicons?domain=adkami.com
+// @grant        none
+// @license      MIT
+// ==/UserScript==
+(function() {
+    'use strict';
+    const elems = document.getElementsByClassName("video-item-list")
+    var to_remove = [];
+    for (var i = 0; i < elems.length; i++) {
+        if (elems.item(i).textContent.toLocaleLowerCase().includes(' vf ')) {
+            to_remove.push(elems.item(i));
+        }
+    }
+    to_remove.forEach(elem => {
+        $(elem).remove();
+    });
+
+    if (elems.length > 0) {
+        let req = new Request("https://www.adkami.com/api/main?objet=adk-mal-all")
+        fetch(req)
+            .then(response => response.json())
+            .then(data => {
+                for (let i = 0; i < elems.length; i++) {
+                    let after = elems[i].getElementsByClassName("right list-edition")[0];
+                    let id = after.dataset["info"].split(",")[0];
+                    let url = data.data.find(el => el["anime_id"] == id);
+                    if (url !== undefined) {
+                        let clickable = document.createElement("a");
+                        clickable.href = "https://myanimelist.net/anime/" + url["mal_id"];
+                        clickable.classList.add("lecteur-icon");
+                        clickable.classList.add("crunchyroll");
+                        let el = document.createElement("img");
+                        clickable.appendChild(el);
+                        el.style = "width: 40px";
+                        el.src = "https://image.myanimelist.net/ui/OK6W_koKDTOqqqLDbIoPAiC8a86sHufn_jOI-JGtoCQ"
+                        elems[i].insertBefore(clickable, after.nextSibling.nextSibling.nextSibling);
+                    }
+                }
+            })
+            .catch(console.error);
+    }
+
+    if (window.location.href.toLowerCase().includes("/anime/")) {
+        var adk_id = window.location.href.match(/anime\/(\d+)/)[1];
+        let req = new Request("https://www.adkami.com/api/main?objet=adk-mal-all")
+        fetch(req)
+            .then(response => response.json())
+            .then(data => {
+                let url = data.data.find(el => el["anime_id"] == adk_id);
+                var ici = document.getElementsByClassName("anime-information-icon")[0];
+                        let clickable = document.createElement("a");
+                        clickable.href = "https://myanimelist.net/anime/" + url["mal_id"];
+                        let el = document.createElement("img");
+                        clickable.appendChild(el);
+                        el.style = "width: 40px";
+                        el.src = "https://image.myanimelist.net/ui/OK6W_koKDTOqqqLDbIoPAiC8a86sHufn_jOI-JGtoCQ"
+                ici.appendChild(clickable);
+        })
+    }
+
+    if (window.location.href.toLowerCase().includes("agenda")) {
+        const agenda = document.getElementsByClassName("col-12 episode");
+        var to_delete = [];
+        for (var u = 0; u < agenda.length; u++) {
+            if (agenda.item(u).textContent.toLocaleLowerCase().includes(' vf')) {
+                to_delete.push(agenda.item(u));
+            }
+        }
+        to_delete.forEach(elem => {
+            $(elem).remove();
+        });
+    }
+})();
