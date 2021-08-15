dfc63bdceced0aac59d491a4397c737a6a69d29e
Zenrac <zenrac@outlook.fr>
2021-08-15 16:01:07 +0200

    Bump ver

--- js/mavanimenextpreviousepbuttons.user.js
+++ js/mavanimenextpreviousepbuttons.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         MavAnime next previous
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       Zenrac
 // @match        http://www.mavanimes.co/*
