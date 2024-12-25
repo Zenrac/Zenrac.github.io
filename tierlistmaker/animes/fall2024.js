9ce9c57b2b577b62af7a1d2276c25a5742eaa648
Zenrac <zenrac@outlook.fr>
2024-12-25 02:17:14 +0100

    :sparkles: Add fall 2024 openings

---
+++ tierlistmaker/animes/fall2024.js
@@ -0,0 +1,288 @@
+window.animeSeasons = window.animeSeasons || {};
+window.animeSeasons['Fall 2024'] = [
+    {
+      "img": "https://cdn.myanimelist.net/images/anime/1584/143719.webp",
+      "title": "Dandadan",
+      "url": "https://myanimelist.net/anime/57334/Dandadan"
+    },
+    {
+      "img": "https://cdn.myanimelist.net/images/anime/1706/144725.webp",
+      "title": "Re:Zero kara Hajimeru Isekai Seikatsu 3rd Season",
+      "url": "https://myanimelist.net/anime/54857/Re_Zero_kara_Hajimeru_Isekai_Seikatsu_3rd_Season"
+    },
+    {
+      "img": "https://cdn.myanimelist.net/images/anime/1207/146272.webp",
+      "title": "Uzumaki",
+      "url": "https://myanimelist.net/anime/40333/Uzumaki"
+    },
+    {
+      "img": "https://cdn.myanimelist.net/images/anime/1584/144860.webp",
+      "title": "Blue Lock vs. U-20 Japan",
+      "url": "https://myanimelist.net/anime/54865/Blue_Lock_vs_U-20_Japan"
+    },
+    {
+      "img": "https://cdn.myanimelist.net/images/anime/1595/144074.webp",
+      "title": "Bleach: Sennen Kessen-hen - Soukoku-tan",
+      "url": "https://myanimelist.net/anime/56784/Bleach__Sennen_Kessen-hen_-_Soukoku-tan"
+    },
+    {
+      "img": "https://cdn.myanimelist.net/images/anime/1496/147108.webp",
+      "title": "Ao no Hako",
+      "url": "https://myanimelist.net/anime/57181/Ao_no_Hako"
+    },
+    {
+      "img": "https://cdn.myanimelist.net/images/anime/1087/144583.webp",
+      "title": "Seirei Gensouki 2",
+      "url": "https://myanimelist.net/anime/50306/Seirei_Gensouki_2"
+    },
+    {
+      "img": "https://cdn.myanimelist.net/images/anime/1553/145597.webp",
+      "title": "Arifureta Shokugyou de Sekai Saikyou Season 3",
+      "url": "https://myanimelist.net/anime/52995/Arifureta_Shokugyou_de_Sekai_Saikyou_Season_3"
+    },
+    {
+      "img": "https://cdn.myanimelist.net/images/anime/1299/144738.webp",
+      "title": "Dungeon ni Deai wo Motomeru no wa Machigatteiru Darou ka V: Houjou no Megami-hen",
+      "url": "https://myanimelist.net/anime/57066/Dungeon_ni_Deai_wo_Motomeru_no_wa_Machigatteiru_Darou_ka_V__Houjou_no_Megami-hen"
+    },
+    {
+      "img": "https://cdn.myanimelist.net/images/anime/1978/144794.webp",
+      "title": "Shangri-La Frontier: Kusoge Hunter, Kamige ni Idoman to su 2nd Season",
+      "url": "https://myanimelist.net/anime/58572/Shangri-La_Frontier__Kusoge_Hunter_Kamige_ni_Idoman_to_su_2nd_Season"
+    },
+    {
+      "img": "https://cdn.myanimelist.net/images/anime/1749/145922.webp",
+      "title": "Chi. Chikyuu no Undou ni Tsuite",
+      "url": "https://myanimelist.net/anime/52215/Chi_Chikyuu_no_Undou_ni_Tsuite"
+    },
+    {
+      "img": "https://cdn.myanimelist.net/images/anime/1155/144299.webp",
+      "title": "Ranma ½ (2024)",
+      "url": "https://myanimelist.net/anime/59145/Ranma_%C2%BD_2024"
+    },
+    {
+      "img": "https://cdn.myanimelist.net/images/anime/1909/144684.webp",
+      "title": "Kimi wa Meido-sama.",
+      "url": "https://myanimelist.net/anime/57611/Kimi_wa_Meido-sama"
+    },
+    {
+      "img": "https://cdn.myanimelist.net/images/anime/1335/145505.webp",
+      "title": "Sword Art Online Alternative: Gun Gale Online II",
+      "url": "https://myanimelist.net/anime/55994/Sword_Art_Online_Alternative__Gun_Gale_Online_II"
+    },
+    {
+      "img": "https://cdn.myanimelist.net/images/anime/1428/143773.webp",
+      "title": "Raise wa Tanin ga Ii",
+      "url": "https://myanimelist.net/anime/56964/Raise_wa_Tanin_ga_Ii"
+    },
+    {
+      "img": "https://cdn.myanimelist.net/images/anime/1723/145231.webp",
+      "title": "Dragon Ball Daima",
+      "url": "https://myanimelist.net/anime/56894/Dragon_Ball_Daima"
+    },
+    {
+      "img": "https://cdn.myanimelist.net/images/anime/1714/145320.webp",
+      "title": "Hitoribocchi no Isekai Kouryaku",
+      "url": "https://myanimelist.net/anime/57891/Hitoribocchi_no_Isekai_Kouryaku"
+    },
+    {
+      "img": "https://cdn.myanimelist.net/images/anime/1679/145660.webp",
+      "title": "Nageki no Bourei wa Intai shitai",
+      "url": "https://myanimelist.net/anime/58172/Nageki_no_Bourei_wa_Intai_shitai"
+    },
+    {
+      "img": "https://cdn.myanimelist.net/images/anime/1280/143705.webp",
+      "title": "Tensei Kizoku, Kantei Skill de Nariagaru 2nd Season",
+      "url": "https://myanimelist.net/anime/59131/Tensei_Kizoku_Kantei_Skill_de_Nariagaru_2nd_Season"
+    },
+    {
+      "img": "https://cdn.myanimelist.net/images/anime/1005/145339.webp",
+      "title": "Rekishi ni Nokoru Akujo ni Naru zo",
+      "url": "https://myanimelist.net/anime/56228/Rekishi_ni_Nokoru_Akujo_ni_Naru_zo"
+    },
+    {
+      "img": "https://cdn.myanimelist.net/images/anime/1675/144605.webp",
+      "title": "Maou 2099",
+      "url": "https://myanimelist.net/anime/54853/Maou_2099"
+    },
+    {
+      "img": "https://cdn.myanimelist.net/images/anime/1617/144043.webp",
+      "title": "Ao no Exorcist: Yuki no Hate-hen",
+      "url": "https://myanimelist.net/anime/58516/Ao_no_Exorcist__Yuki_no_Hate-hen"
+    },
+    {
+      "img": "https://cdn.myanimelist.net/images/anime/1410/143586.webp",
+      "title": "Amagami-san Chi no Enmusubi",
+      "url": "https://myanimelist.net/anime/55071/Amagami-san_Chi_no_Enmusubi"
+    },
+    {
+      "img": "https://cdn.myanimelist.net/images/anime/1878/145376.webp",
+      "title": "Yarinaoshi Reijou wa Ryuutei Heika wo Kouryakuchuu",
+      "url": "https://myanimelist.net/anime/55150/Yarinaoshi_Reijou_wa_Ryuutei_Heika_wo_Kouryakuchuu"
+    },
+    {
+      "img": "https://cdn.myanimelist.net/images/anime/1225/144606.webp",
+      "title": "Sayounara Ryuusei, Konnichiwa Jinsei",
+      "url": "https://myanimelist.net/anime/58445/Sayounara_Ryuusei_Konnichiwa_Jinsei"
+    },
+    {
+      "img": "https://cdn.myanimelist.net/images/anime/1721/145335.webp",
+      "title": "Party kara Tsuihou sareta Sono Chiyushi, Jitsu wa Saikyou ni Tsuki",
+      "url": "https://myanimelist.net/anime/57944/Party_kara_Tsuihou_sareta_Sono_Chiyushi_Jitsu_wa_Saikyou_ni_Tsuki"
+    },
+    {
+      "img": "https://cdn.myanimelist.net/images/anime/1752/145548.webp",
+      "title": "Tsuma, Shougakusei ni Naru.",
+      "url": "https://myanimelist.net/anime/54726/Tsuma_Shougakusei_ni_Naru"
+    },
+    {
+      "img": "https://cdn.myanimelist.net/images/anime/1537/145325.webp",
+      "title": "NegaPosi Angler",
+      "url": "https://myanimelist.net/anime/59425/NegaPosi_Angler"
+    },
+    {
+      "img": "https://cdn.myanimelist.net/images/anime/1436/144553.webp",
+      "title": "Hoshifuru Oukoku no Nina",
+      "url": "https://myanimelist.net/anime/57362/Hoshifuru_Oukoku_no_Nina"
+    },
+    {
+      "img": "https://cdn.myanimelist.net/images/anime/1667/145393.webp",
+      "title": "Maou-sama, Retry! R",
+      "url": "https://myanimelist.net/anime/56400/Maou-sama_Retry_R"
+    },
+    {
+      "img": "https://cdn.myanimelist.net/images/anime/1910/144517.webp",
+      "title": "Houkago Shounen Hanako-kun Part 2",
+      "url": "https://myanimelist.net/anime/57102/Houkago_Shounen_Hanako-kun_Part_2"
+    },
+    {
+      "img": "https://cdn.myanimelist.net/images/anime/1037/145338.webp",
+      "title": "MF Ghost 2nd Season",
+      "url": "https://myanimelist.net/anime/57559/MF_Ghost_2nd_Season"
+    },
+    {
+      "img": "https://cdn.myanimelist.net/images/anime/1928/146063.webp",
+      "title": "One Piece: Gyojin Tou-hen",
+      "url": "https://myanimelist.net/anime/60108/One_Piece__Gyojin_Tou-hen"
+    },
+    {
+      "img": "https://cdn.myanimelist.net/images/anime/1109/145389.webp",
+      "title": "Nanatsu no Taizai: Mokushiroku no Yonkishi 2nd Season",
+      "url": "https://myanimelist.net/anime/58511/Nanatsu_no_Taizai__Mokushiroku_no_Yonkishi_2nd_Season"
+    },
+    {
+      "img": "https://cdn.myanimelist.net/images/anime/1659/143938.webp",
+      "title": "Ao no Miburo",
+      "url": "https://myanimelist.net/anime/56647/Ao_no_Miburo"
+    },
+    {
+      "img": "https://cdn.myanimelist.net/images/anime/1926/145102.webp",
+      "title": "Kabushikigaisha Magi-Lumière",
+      "url": "https://myanimelist.net/anime/57360/Kabushikigaisha_Magi-Lumi%C3%A8re"
+    },
+    {
+      "img": "https://cdn.myanimelist.net/images/anime/1800/145662.webp",
+      "title": "Youkai Gakkou no Sensei Hajimemashita!",
+      "url": "https://myanimelist.net/anime/57533/Youkai_Gakkou_no_Sensei_Hajimemashita"
+    },
+    {
+      "img": "https://cdn.myanimelist.net/images/anime/1397/145153.webp",
+      "title": "Acro Trip",
+      "url": "https://myanimelist.net/anime/53723/Acro_Trip"
+    },
+    {
+      "img": "https://cdn.myanimelist.net/images/anime/1597/145178.webp",
+      "title": "Trillion Game",
+      "url": "https://myanimelist.net/anime/56662/Trillion_Game"
+    },
+    {
+      "img": "https://cdn.myanimelist.net/images/anime/1489/144640.webp",
+      "title": "Love Live! Superstar!! 3rd Season",
+      "url": "https://myanimelist.net/anime/53287/Love_Live_Superstar_3rd_Season"
+    },
+    {
+      "img": "https://cdn.myanimelist.net/images/anime/1306/145571.webp",
+      "title": "Touhai: Ura Rate Mahjong Touhairoku",
+      "url": "https://myanimelist.net/anime/57796/Touhai__Ura_Rate_Mahjong_Touhairoku"
+    },
+    {
+      "img": "https://cdn.myanimelist.net/images/anime/1535/145026.webp",
+      "title": "Mahoutsukai ni Narenakatta Onnanoko no Hanashi",
+      "url": "https://myanimelist.net/anime/58173/Mahoutsukai_ni_Narenakatta_Onnanoko_no_Hanashi"
+    },
+    {
+      "img": "https://cdn.myanimelist.net/images/anime/1214/142185.webp",
+      "title": "Haigakura",
+      "url": "https://myanimelist.net/anime/56420/Haigakura"
+    },
+    {
+      "img": "https://cdn.myanimelist.net/images/anime/1681/145175.webp",
+      "title": "Puniru wa Kawaii Slime",
+      "url": "https://myanimelist.net/anime/57517/Puniru_wa_Kawaii_Slime"
+    },
+    {
+      "img": "https://cdn.myanimelist.net/images/anime/1954/144622.webp",
+      "title": "Kinoko Inu",
+      "url": "https://myanimelist.net/anime/58854/Kinoko_Inu"
+    },
+    {
+      "img": "https://cdn.myanimelist.net/images/anime/1475/145570.webp",
+      "title": "Kamierabi 2nd Season",
+      "url": "https://myanimelist.net/anime/56967/Kamierabi_2nd_Season"
+    },
+    {
+      "img": "https://cdn.myanimelist.net/images/anime/1713/144761.webp",
+      "title": "Hamidashi Creative",
+      "url": "https://myanimelist.net/anime/56461/Hamidashi_Creative"
+    },
+    {
+      "img": "https://cdn.myanimelist.net/images/anime/1507/145670.webp",
+      "title": "Shin Tennis no Oujisama: U-17 World Cup Semifinal",
+      "url": "https://myanimelist.net/anime/55570/Shin_Tennis_no_Oujisama__U-17_World_Cup_Semifinal"
+    },
+    {
+      "img": "https://cdn.myanimelist.net/images/anime/1509/144685.webp",
+      "title": "Kankin Kuiki Level X",
+      "url": "https://myanimelist.net/anime/59505/Kankin_Kuiki_Level_X"
+    },
+    {
+      "img": "https://cdn.myanimelist.net/images/anime/1087/144083.webp",
+      "title": "Fairy Tail: 100-nen Quest",
+      "url": "https://myanimelist.net/anime/49785/Fairy_Tail__100-nen_Quest"
+    },
+    {
+      "img": "https://cdn.myanimelist.net/images/anime/1779/143584.webp",
+      "title": "2.5-jigen no Ririsa",
+      "url": "https://myanimelist.net/anime/53802/25-jigen_no_Ririsa"
+    },
+    {
+      "img": "https://cdn.myanimelist.net/images/anime/1660/143460.webp",
+      "title": "Kimi to Boku no Saigo no Senjou, Aruiwa Sekai ga Hajimaru Seisen Season II",
+      "url": "https://myanimelist.net/anime/49981/Kimi_to_Boku_no_Saigo_no_Senjou_Aruiwa_Sekai_ga_Hajimaru_Seisen_Season_II"
+    },
+    {
+      "img": "https://cdn.myanimelist.net/images/anime/1455/141858.webp",
+      "title": "Sasayaku You ni Koi wo Utau",
+      "url": "https://myanimelist.net/anime/54233/Sasayaku_You_ni_Koi_wo_Utau"
+    },
+    {
+      "img": "https://cdn.myanimelist.net/images/anime/1106/145996.webp",
+      "title": "Delico's Nursery",
+      "url": "https://myanimelist.net/anime/56179/Delicos_Nursery"
+    },
+    {
+      "img": "https://cdn.myanimelist.net/images/anime/1482/143629.webp",
+      "title": "Tasuuketsu",
+      "url": "https://myanimelist.net/anime/57945/Tasuuketsu"
+    },
+    {
+      "img": "https://cdn.myanimelist.net/images/anime/1948/143550.webp",
+      "title": "Sengoku Youko: Senma Konton-hen",
+      "url": "https://myanimelist.net/anime/58488/Sengoku_Youko__Senma_Konton-hen"
+    },
+    {
+      "img": "https://cdn.myanimelist.net/images/anime/1852/140786.webp",
+      "title": "Wonderful Precure!",
+      "url": "https://myanimelist.net/anime/57390/Wonderful_Precure"
+    }
+  ]
