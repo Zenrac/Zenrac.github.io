c78d697dfb03688cbf73dfc0a421b63602185d32
Zenrac <zenrac@outlook.fr>
2025-06-10 21:48:53 +0200

    :sparkles: Add Spring 2025 animes

---
+++ tierlistmaker/animes/spring2025.js
@@ -0,0 +1,268 @@
+window.animeSeasons = window.animeSeasons || {};
+window.animeSeasons['Spring 2025'] = [
+  {
+    "img": "https://cdn.myanimelist.net/images/anime/1025/147458.jpg",
+    "title": "Kusuriya no Hitorigoto 2nd Season",
+    "url": "https://myanimelist.net/anime/58514/Kusuriya_no_Hitorigoto_2nd_Season"
+  },
+  {
+    "img": "https://cdn.myanimelist.net/images/anime/1527/146836.jpg",
+    "title": "Enen no Shouboutai: San no Shou",
+    "url": "https://myanimelist.net/anime/51818/Enen_no_Shouboutai__San_no_Shou"
+  },
+  {
+    "img": "https://cdn.myanimelist.net/images/anime/1098/150300.jpg",
+    "title": "Lazarus",
+    "url": "https://myanimelist.net/anime/56038/Lazarus"
+  },
+  {
+    "img": "https://cdn.myanimelist.net/images/anime/1526/148873.jpg",
+    "title": "Wind Breaker Season 2",
+    "url": "https://myanimelist.net/anime/59160/Wind_Breaker_Season_2"
+  },
+  {
+    "img": "https://cdn.myanimelist.net/images/anime/1988/148017.jpg",
+    "title": "Witch Watch",
+    "url": "https://myanimelist.net/anime/59597/Witch_Watch"
+  },
+  {
+    "img": "https://cdn.myanimelist.net/images/anime/1712/148299.jpg",
+    "title": "Saikyou no Ousama, Nidome no Jinsei wa Nani wo Suru?",
+    "url": "https://myanimelist.net/anime/60146/Saikyou_no_Ousama_Nidome_no_Jinsei_wa_Nani_wo_Suru"
+  },
+  {
+    "img": "https://cdn.myanimelist.net/images/anime/1538/148604.jpg",
+    "title": "Vigilante: Boku no Hero Academia Illegals",
+    "url": "https://myanimelist.net/anime/60593/Vigilante__Boku_no_Hero_Academia_Illegals"
+  },
+  {
+    "img": "https://cdn.myanimelist.net/images/anime/1719/150050.jpg",
+    "title": "Kowloon Generic Romance",
+    "url": "https://myanimelist.net/anime/60083/Kowloon_Generic_Romance"
+  },
+  {
+    "img": "https://cdn.myanimelist.net/images/anime/1069/148148.jpg",
+    "title": "Katainaka no Ossan, Kensei ni Naru",
+    "url": "https://myanimelist.net/anime/59452/Katainaka_no_Ossan_Kensei_ni_Naru"
+  },
+  {
+    "img": "https://cdn.myanimelist.net/images/anime/1074/147339.jpg",
+    "title": "Slime Taoshite 300-nen, Shiranai Uchi ni Level Max ni Nattemashita: Sono Ni",
+    "url": "https://myanimelist.net/anime/50738/Slime_Taoshite_300-nen_Shiranai_Uchi_ni_Level_Max_ni_Nattemashita__Sono_Ni"
+  },
+  {
+    "img": "https://cdn.myanimelist.net/images/anime/1429/150067.jpg",
+    "title": "Danjo no Yuujou wa Seiritsu suru? (Iya, Shinai!!)",
+    "url": "https://myanimelist.net/anime/52709/Danjo_no_Yuujou_wa_Seiritsu_suru_Iya_Shinai"
+  },
+  {
+    "img": "https://cdn.myanimelist.net/images/anime/1660/143460.jpg",
+    "title": "Kimi to Boku no Saigo no Senjou, Aruiwa Sekai ga Hajimaru Seisen Season II",
+    "url": "https://myanimelist.net/anime/49981/Kimi_to_Boku_no_Saigo_no_Senjou_Aruiwa_Sekai_ga_Hajimaru_Seisen_Season_II"
+  },
+  {
+    "img": "https://cdn.myanimelist.net/images/anime/1521/148809.jpg",
+    "title": "Haite Kudasai, Takamine-san",
+    "url": "https://myanimelist.net/anime/59457/Haite_Kudasai_Takamine-san"
+  },
+  {
+    "img": "https://cdn.myanimelist.net/images/anime/1722/148906.jpg",
+    "title": "Kijin Gentoushou",
+    "url": "https://myanimelist.net/anime/49778/Kijin_Gentoushou"
+  },
+  {
+    "img": "https://cdn.myanimelist.net/images/anime/1211/147335.jpg",
+    "title": "Isshun de Chiryou shiteita noni Yakutatazu to Tsuihou sareta Tensai Chiyushi, Yami Healer toshite Tanoshiku Ikiru",
+    "url": "https://myanimelist.net/anime/58359/Isshun_de_Chiryou_shiteita_noni_Yakutatazu_to_Tsuihou_sareta_Tensai_Chiyushi_Yami_Healer_toshite_Tanoshiku_Ikiru"
+  },
+  {
+    "img": "https://cdn.myanimelist.net/images/anime/1897/149800.jpg",
+    "title": "A-Rank Party wo Ridatsu shita Ore wa, Moto Oshiego-tachi to Meikyuu Shinbu wo Mezasu.",
+    "url": "https://myanimelist.net/anime/59730/A-Rank_Party_wo_Ridatsu_shita_Ore_wa_Moto_Oshiego-tachi_to_Meikyuu_Shinbu_wo_Mezasu"
+  },
+  {
+    "img": "https://cdn.myanimelist.net/images/anime/1955/148360.jpg",
+    "title": "Shiunji-ke no Kodomotachi",
+    "url": "https://myanimelist.net/anime/58131/Shiunji-ke_no_Kodomotachi"
+  },
+  {
+    "img": "https://cdn.myanimelist.net/images/anime/1501/148355.jpg",
+    "title": "Aharen-san wa Hakarenai Season 2",
+    "url": "https://myanimelist.net/anime/59466/Aharen-san_wa_Hakarenai_Season_2"
+  },
+  {
+    "img": "https://cdn.myanimelist.net/images/anime/1405/147694.jpg",
+    "title": "Sentai Daishikkaku 2nd Season",
+    "url": "https://myanimelist.net/anime/59189/Sentai_Daishikkaku_2nd_Season"
+  },
+  {
+    "img": "https://cdn.myanimelist.net/images/anime/1743/148272.jpg",
+    "title": "Kanchigai no Atelier Meister: Eiyuu Party no Moto Zatsuyougakari ga, Jitsu wa Sentou Igai ga SSS Rank Datta to Iu Yoku Aru Hanashi",
+    "url": "https://myanimelist.net/anime/60140/Kanchigai_no_Atelier_Meister__Eiyuu_Party_no_Moto_Zatsuyougakari_ga_Jitsu_wa_Sentou_Igai_ga_SSS_Rank_Datta_to_Iu_Yoku_Aru_Hanashi"
+  },
+  {
+    "img": "https://cdn.myanimelist.net/images/anime/1691/148602.jpg",
+    "title": "Summer Pockets",
+    "url": "https://myanimelist.net/anime/50694/Summer_Pockets"
+  },
+  {
+    "img": "https://cdn.myanimelist.net/images/anime/1703/148600.jpg",
+    "title": "Ore wa Seikan Kokka no Akutoku Ryoushu!",
+    "url": "https://myanimelist.net/anime/60154/Ore_wa_Seikan_Kokka_no_Akutoku_Ryoushu"
+  },
+  {
+    "img": "https://cdn.myanimelist.net/images/anime/1781/150071.jpg",
+    "title": "Rock wa Lady no Tashinami deshite",
+    "url": "https://myanimelist.net/anime/59360/Rock_wa_Lady_no_Tashinami_deshite"
+  },
+  {
+    "img": "https://cdn.myanimelist.net/images/anime/1263/148318.jpg",
+    "title": "Kanpekisugite Kawaige ga Nai to Konyaku Haki sareta Seijo wa Ringoku ni Urareru",
+    "url": "https://myanimelist.net/anime/60157/Kanpekisugite_Kawaige_ga_Nai_to_Konyaku_Haki_sareta_Seijo_wa_Ringoku_ni_Urareru"
+  },
+  {
+    "img": "https://cdn.myanimelist.net/images/anime/1517/148292.jpg",
+    "title": "Kuroshitsuji: Midori no Majo-hen",
+    "url": "https://myanimelist.net/anime/59228/Kuroshitsuji__Midori_no_Majo-hen"
+  },
+  {
+    "img": "https://cdn.myanimelist.net/images/anime/1052/148743.jpg",
+    "title": "Kidou Senshi Gundam: GQuuuuuuX",
+    "url": "https://myanimelist.net/anime/60449/Kidou_Senshi_Gundam__GQuuuuuuX"
+  },
+  {
+    "img": "https://cdn.myanimelist.net/images/anime/1843/146935.jpg",
+    "title": "Kakushite! Makina-san!!",
+    "url": "https://myanimelist.net/anime/58833/Kakushite_Makina-san"
+  },
+  {
+    "img": "https://cdn.myanimelist.net/images/anime/1620/148221.jpg",
+    "title": "Aru Majo ga Shinu Made",
+    "url": "https://myanimelist.net/anime/59169/Aru_Majo_ga_Shinu_Made"
+  },
+  {
+    "img": "https://cdn.myanimelist.net/images/anime/1714/148317.jpg",
+    "title": "Gorilla no Kami kara Kago sareta Reijou wa Ouritsu Kishidan de Kawaigarareru",
+    "url": "https://myanimelist.net/anime/59935/Gorilla_no_Kami_kara_Kago_sareta_Reijou_wa_Ouritsu_Kishidan_de_Kawaigarareru"
+  },
+  {
+    "img": "https://cdn.myanimelist.net/images/anime/1564/150049.jpg",
+    "title": "Anne Shirley",
+    "url": "https://myanimelist.net/anime/60334/Anne_Shirley"
+  },
+  {
+    "img": "https://cdn.myanimelist.net/images/anime/1224/148321.jpg",
+    "title": "Shoushimin Series 2nd Season",
+    "url": "https://myanimelist.net/anime/59828/Shoushimin_Series_2nd_Season"
+  },
+  {
+    "img": "https://cdn.myanimelist.net/images/anime/1577/147967.jpg",
+    "title": "Your Forma",
+    "url": "https://myanimelist.net/anime/55995/Your_Forma"
+  },
+  {
+    "img": "https://cdn.myanimelist.net/images/anime/1855/146642.jpg",
+    "title": "Ninja to Koroshiya no Futarigurashi",
+    "url": "https://myanimelist.net/anime/58725/Ninja_to_Koroshiya_no_Futarigurashi"
+  },
+  {
+    "img": "https://cdn.myanimelist.net/images/anime/1511/148642.jpg",
+    "title": "Apocalypse Hotel",
+    "url": "https://myanimelist.net/anime/59675/Apocalypse_Hotel"
+  },
+  {
+    "img": "https://cdn.myanimelist.net/images/anime/1073/149267.jpg",
+    "title": "Shin Samurai-den Yaiba",
+    "url": "https://myanimelist.net/anime/58812/Shin_Samurai-den_Yaiba"
+  },
+  {
+    "img": "https://cdn.myanimelist.net/images/anime/1677/148603.jpg",
+    "title": "Mono",
+    "url": "https://myanimelist.net/anime/58492/Mono"
+  },
+  {
+    "img": "https://cdn.myanimelist.net/images/anime/1944/148218.jpg",
+    "title": "Zatsu Tabi: That's Journey",
+    "url": "https://myanimelist.net/anime/55514/Zatsu_Tabi__Thats_Journey"
+  },
+  {
+    "img": "https://cdn.myanimelist.net/images/anime/1065/148659.jpg",
+    "title": "Ballpark de Tsukamaete!",
+    "url": "https://myanimelist.net/anime/60293/Ballpark_de_Tsukamaete"
+  },
+  {
+    "img": "https://cdn.myanimelist.net/images/anime/1626/148097.jpg",
+    "title": "Uma Musume: Cinderella Gray",
+    "url": "https://myanimelist.net/anime/59636/Uma_Musume__Cinderella_Gray"
+  },
+  {
+    "img": "https://cdn.myanimelist.net/images/anime/1221/148863.jpg",
+    "title": "Hibi wa Sugiredo Meshi Umashi",
+    "url": "https://myanimelist.net/anime/60658/Hibi_wa_Sugiredo_Meshi_Umashi"
+  },
+  {
+    "img": "https://cdn.myanimelist.net/images/anime/1118/149222.jpg",
+    "title": "Nazotoki wa Dinner no Ato de",
+    "url": "https://myanimelist.net/anime/60377/Nazotoki_wa_Dinner_no_Ato_de"
+  },
+  {
+    "img": "https://cdn.myanimelist.net/images/anime/1066/148358.jpg",
+    "title": "Teogonia",
+    "url": "https://myanimelist.net/anime/58614/Teogonia"
+  },
+  {
+    "img": "https://cdn.myanimelist.net/images/anime/1750/150046.jpg",
+    "title": "Guilty Gear Strive: Dual Rulers",
+    "url": "https://myanimelist.net/anime/59070/Guilty_Gear_Strive__Dual_Rulers"
+  },
+  {
+    "img": "https://cdn.myanimelist.net/images/anime/1538/147930.jpg",
+    "title": "Bye Bye, Earth 2nd Season",
+    "url": "https://myanimelist.net/anime/59819/Bye_Bye_Earth_2nd_Season"
+  },
+  {
+    "img": "https://cdn.myanimelist.net/images/anime/1214/142185.jpg",
+    "title": "Haigakura",
+    "url": "https://myanimelist.net/anime/56420/Haigakura"
+  },
+  {
+    "img": "https://cdn.myanimelist.net/images/anime/1770/148195.jpg",
+    "title": "Chuuzenji-sensei Mononoke Kougiroku: Sensei ga Nazo wo Hodoite Shimau kara.",
+    "url": "https://myanimelist.net/anime/60009/Chuuzenji-sensei_Mononoke_Kougiroku__Sensei_ga_Nazo_wo_Hodoite_Shimau_kara"
+  },
+  {
+    "img": "https://cdn.myanimelist.net/images/anime/1907/148540.jpg",
+    "title": "Maebashi Witches",
+    "url": "https://myanimelist.net/anime/59744/Maebashi_Witches"
+  },
+  {
+    "img": "https://cdn.myanimelist.net/images/anime/1979/148096.jpg",
+    "title": "Uchuujin MuuMuu",
+    "url": "https://myanimelist.net/anime/60401/Uchuujin_MuuMuu"
+  },
+  {
+    "img": "https://cdn.myanimelist.net/images/anime/1678/148543.jpg",
+    "title": "#Compass 2.0: Sentou Setsuri Kaiseki System",
+    "url": "https://myanimelist.net/anime/53762/Compass_20__Sentou_Setsuri_Kaiseki_System"
+  },
+  {
+    "img": "https://cdn.myanimelist.net/images/anime/1231/149888.jpg",
+    "title": "Shirobuta Kizoku desu ga Zense no Kioku ga Haeta node Hiyoko na Otouto Sodatemasu",
+    "url": "https://myanimelist.net/anime/59322/Shirobuta_Kizoku_desu_ga_Zense_no_Kioku_ga_Haeta_node_Hiyoko_na_Otouto_Sodatemasu"
+  },
+  {
+    "img": "https://cdn.myanimelist.net/images/anime/1793/150282.jpg",
+    "title": "Everyday Host",
+    "url": "https://myanimelist.net/anime/60535/Everyday_Host"
+  },
+  {
+    "img": "https://cdn.myanimelist.net/images/anime/1831/149970.jpg",
+    "title": "Princession Orchestra",
+    "url": "https://myanimelist.net/anime/59078/Princession_Orchestra"
+  },
+  {
+    "img": "https://cdn.myanimelist.net/images/anime/1277/147358.jpg",
+    "title": "Kimi to Idol Precure♪",
+    "url": "https://myanimelist.net/anime/60407/Kimi_to_Idol_Precure%E2%99%AA"
+  }
+]
